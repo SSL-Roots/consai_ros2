afb1c08b92b67b2b468f5e7c458d4c51047f3ff7
ShotaAk <macakasit@gmail.com>
2021-11-23 16:27:42 +0900

    game.pyのリファクタリング

--- consai_examples/consai_examples/game.py
+++ consai_examples/consai_examples/game.py
@@ -47,92 +47,92 @@ def main():
     prev_ball_placement_state = -1  # ボール配置状態更新判定用の変数
     while rclpy.ok():
         updated_roles = []
-        referee_has_changed = prev_referee_command != referee_parser.get_command()
-        ball_state_has_changed = prev_ball_state != field_observer.get_ball_state()
+        referee_has_changed = prev_referee_command != referee.get_command()
+        ball_state_has_changed = prev_ball_state != observer.get_ball_state()
 
         # レフェリーコマンドが変化した場合は、全てのロボットの行動を更新する
         if referee_has_changed:
             print("referee command has changed")
-            prev_referee_command = referee_parser.get_command()
-            updated_roles = assignment_node.get_active_roles()
+            prev_referee_command = referee.get_command()
+            updated_roles = assignor.get_active_roles()
 
         # ボール状態が変化した場合は、すべてのロボットの行動を更新する
         if ball_state_has_changed:
             print("ball state has changed")
-            prev_ball_state = field_observer.get_ball_state()
-            updated_roles = assignment_node.get_active_roles()
+            prev_ball_state = observer.get_ball_state()
+            updated_roles = assignor.get_active_roles()
 
         # ボール配置状態が変化した場合は、すべてのロボットの行動を更新する
-        if referee_parser.our_ball_placement() or referee_parser.their_ball_placement():
-            if prev_ball_placement_state != field_observer.get_ball_placement_state(referee_parser.placement_position()):
+        if referee.our_ball_placement() or referee.their_ball_placement():
+            if prev_ball_placement_state != observer.get_ball_placement_state(referee.placement_position()):
                 print("ball placement state has changed")
-                prev_ball_placement_state = field_observer.get_ball_placement_state(referee_parser.placement_position())
-                updated_roles = assignment_node.get_active_roles()
+                prev_ball_placement_state = observer.get_ball_placement_state(referee.placement_position())
+                updated_roles = assignor.get_active_roles()
 
         # アタッカーの切り替わりを防ぐため、
         # ボールが動いてたり、ディフェンスエリアにあるときは役割を更新しない
         if not referee_has_changed and \
            not ball_state_has_changed and \
-           not field_observer.ball_is_in_our_defense_area() and \
-           not field_observer.ball_is_moving():
+           not observer.ball_is_in_our_defense_area() and \
+           not observer.ball_is_moving():
             # ロボットの役割の更新し、
             # 役割が変わったロボットのみ、行動を更新する
-            updated_roles = assignment_node.update_role()
+            updated_roles = assignor.update_role()
 
         for role in updated_roles:
-            robot_id = assignment_node.get_robot_id(role)
+            robot_id = assignor.get_robot_id(role)
             # ボール状態をセットする
             decisions[role].set_ball_state(prev_ball_state)
             # ボール配置状態をセットする
             decisions[role].set_ball_placement_state(prev_ball_placement_state)
 
             # レフェリーコマンドに合わせて行動を決定する
-            if field_observer.ball_is_outside():
+            if observer.ball_is_outside():
                 # ボールが場外に出たらロボットを停止する
                 decisions[role].halt(robot_id)
                 print("ボールが外に出た")
                 continue
 
-            if referee_parser.halt():
+            if referee.halt():
                 decisions[role].halt(robot_id)
-            elif referee_parser.stop():
+            elif referee.stop():
                 decisions[role].stop(robot_id)
-            elif referee_parser.inplay():
+            elif referee.inplay():
                 decisions[role].inplay(robot_id)
-            elif referee_parser.our_pre_kickoff():
+            elif referee.our_pre_kickoff():
                 decisions[role].our_pre_kickoff(robot_id)
-            elif referee_parser.our_kickoff():
+            elif referee.our_kickoff():
                 decisions[role].our_kickoff(robot_id)
-            elif referee_parser.their_pre_kickoff():
+            elif referee.their_pre_kickoff():
                 decisions[role].their_pre_kickoff(robot_id)
-            elif referee_parser.their_kickoff():
+            elif referee.their_kickoff():
                 decisions[role].their_kickoff(robot_id)
-            elif referee_parser.our_pre_penalty():
+            elif referee.our_pre_penalty():
                 decisions[role].our_pre_penalty(robot_id)
-            elif referee_parser.our_penalty():
+            elif referee.our_penalty():
                 decisions[role].our_penalty(robot_id)
-            elif referee_parser.their_pre_penalty():
+            elif referee.their_pre_penalty():
                 decisions[role].their_pre_penalty(robot_id)
-            elif referee_parser.their_penalty():
+            elif referee.their_penalty():
                 decisions[role].their_penalty(robot_id)
-            elif referee_parser.our_direct():
+            elif referee.our_direct():
                 decisions[role].our_direct(robot_id)
-            elif referee_parser.their_direct():
+            elif referee.their_direct():
                 decisions[role].their_direct(robot_id)
-            elif referee_parser.our_indirect():
+            elif referee.our_indirect():
                 decisions[role].our_indirect(robot_id)
-            elif referee_parser.their_indirect():
+            elif referee.their_indirect():
                 decisions[role].their_indirect(robot_id)
-            elif referee_parser.our_timeout():
+            elif referee.our_timeout():
                 decisions[role].our_timeout(robot_id)
-            elif referee_parser.their_timeout():
+            elif referee.their_timeout():
                 decisions[role].their_timeout(robot_id)
-            elif referee_parser.our_ball_placement():
+            elif referee.our_ball_placement():
                 decisions[role].our_ball_placement(
-                    robot_id, referee_parser.placement_position())
-            elif referee_parser.their_ball_placement():
+                    robot_id, referee.placement_position())
+            elif referee.their_ball_placement():
                 decisions[role].their_ball_placement(
-                    robot_id, referee_parser.placement_position())
+                    robot_id, referee.placement_position())
             else:
                 print("UNDEFINED REFEREE COMMAND!!!")
 
@@ -154,33 +154,33 @@ if __name__ == '__main__':
 
     rclpy.init(args=None)
 
-    operator_node = RobotOperator(args.yellow)
-    assignment_node = RoleAssignment(args.goalie, args.yellow)
-    referee_parser = RefereeParser(args.yellow, args.invert)
-    field_observer = FieldObserver()
+    operator = RobotOperator(args.yellow)
+    assignor = RoleAssignment(args.goalie, args.yellow)
+    referee = RefereeParser(args.yellow, args.invert)
+    observer = FieldObserver()
 
     executor = MultiThreadedExecutor()
-    executor.add_node(operator_node)
-    executor.add_node(assignment_node)
-    executor.add_node(referee_parser)
-    executor.add_node(field_observer)
+    executor.add_node(operator)
+    executor.add_node(assignor)
+    executor.add_node(referee)
+    executor.add_node(observer)
 
     # エグゼキュータは別スレッドでspinさせ続ける
     executor_thread = threading.Thread(target=executor.spin, daemon=True)
     executor_thread.start()
 
     decisions = {
-        ROLE_GOALIE: GoaleDecition(operator_node),
-        ROLE_ATTACKER: AttackerDecition(operator_node),
-        ROLE_CENTER_BACK1: DecisionBase(operator_node),
-        ROLE_CENTER_BACK2: DecisionBase(operator_node),
-        ROLE_SUB_ATTACKER: DecisionBase(operator_node),
-        ROLE_ZONE1: DecisionBase(operator_node),
-        ROLE_ZONE2: DecisionBase(operator_node),
-        ROLE_ZONE3: DecisionBase(operator_node),
-        ROLE_ZONE4: DecisionBase(operator_node),
-        ROLE_SIDE_BACK1: DecisionBase(operator_node),
-        ROLE_SIDE_BACK2: DecisionBase(operator_node),
+        ROLE_GOALIE: GoaleDecition(operator),
+        ROLE_ATTACKER: AttackerDecition(operator),
+        ROLE_CENTER_BACK1: DecisionBase(operator),
+        ROLE_CENTER_BACK2: DecisionBase(operator),
+        ROLE_SUB_ATTACKER: DecisionBase(operator),
+        ROLE_ZONE1: DecisionBase(operator),
+        ROLE_ZONE2: DecisionBase(operator),
+        ROLE_ZONE3: DecisionBase(operator),
+        ROLE_ZONE4: DecisionBase(operator),
+        ROLE_SIDE_BACK1: DecisionBase(operator),
+        ROLE_SIDE_BACK2: DecisionBase(operator),
     }
 
     try:
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     print("ロボットを停止します")
     # ロボットを停止
     for i in range(16):
-        operator_node.stop(i)
+        operator.stop(i)
     time.sleep(1.0)
 
     rclpy.shutdown()
