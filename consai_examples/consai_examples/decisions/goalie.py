bdc1a2d93cd39bb19324ae96475430dbd6427702
uchikun2493 <um2493@gmail.com>
2023-04-26 18:49:31 +0900

    act_idの位置を修正

--- consai_examples/consai_examples/decisions/goalie.py
+++ consai_examples/consai_examples/decisions/goalie.py
@@ -73,7 +73,7 @@ class GoaleDecision(DecisionBase):
                         self._operator.shoot_to_their_corner(robot_id, target_is_top_corner=True, set_play=False)
                     else:
                         self._operator.shoot_to_their_corner(robot_id, target_is_top_corner=False, set_play=False)
-                    self._act_id = ID_IN_DEFENSE
+                self._act_id = ID_IN_DEFENSE
             return
 
         if self._act_id != ID_IN_PLAY:
