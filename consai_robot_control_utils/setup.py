6906faf749cec9a194678765ef22cd203215b6b0
Makito Ishikura <tilt.silvie@gmail.com>
2023-04-30 12:07:50 +0900

    control util用のパッケージ作成

---
+++ consai_robot_control_utils/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'consai_robot_control_utils'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='roots',
+    maintainer_email='tilt.silvie@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
