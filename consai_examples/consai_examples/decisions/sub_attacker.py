cfa29e57ebdc85ae1e332032653d9490b15e911f
Shota Aoki <macakasit@gmail.com>
2023-05-03 21:34:19 +0900

    ペナルティ後に相手ディフェンスエリアに侵入しないように、待機位置を変更した (#81)
    * ペナルティ後に相手ディフェンスエリアに侵入しないように、待機位置を変更した
    
    * 待機位置修正
    
    * 変数をdecision_baseに移動

--- consai_examples/consai_examples/decisions/sub_attacker.py
+++ consai_examples/consai_examples/decisions/sub_attacker.py
@@ -94,22 +94,22 @@ class SubAttackerDecision(DecisionBase):
 
     def our_pre_penalty(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_PENALTY:
-            self._operator.move_to_look_ball(robot_id, -6.0 + 0.5, 4.5 - 0.3 * 5.0)
+            self._operator.move_to_look_ball(robot_id, -self._PENALTY_WAIT_X, 4.5 - 0.3 * 5.0)
             self._act_id = self.ACT_ID_PRE_PENALTY
 
     def our_penalty(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_PENALTY:
-            self._operator.move_to_look_ball(robot_id, -6.0 + 0.5, 4.5 - 0.3 * 5.0)
+            self._operator.move_to_look_ball(robot_id, -self._PENALTY_WAIT_X, 4.5 - 0.3 * 5.0)
             self._act_id = self.ACT_ID_PRE_PENALTY
 
     def their_pre_penalty(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_PENALTY:
-            self._operator.move_to_look_ball(robot_id, 6.0 - 0.5, 4.5 - 0.3 * 5.0)
+            self._operator.move_to_look_ball(robot_id, self._PENALTY_WAIT_X, 4.5 - 0.3 * 5.0)
             self._act_id = self.ACT_ID_PRE_PENALTY
 
     def their_penalty(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_PENALTY:
-            self._operator.move_to_look_ball(robot_id, 6.0 - 0.5, 4.5 - 0.3 * 5.0)
+            self._operator.move_to_look_ball(robot_id, self._PENALTY_WAIT_X, 4.5 - 0.3 * 5.0)
             self._act_id = self.ACT_ID_PRE_PENALTY
 
     def our_penalty_inplay(self, robot_id):
