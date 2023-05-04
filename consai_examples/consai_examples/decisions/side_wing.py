ba8f244802dd0774d23e6cdf036b6527312bc6ff
nabekuro <nabekuro0104@gmail.com>
2023-05-04 13:25:47 +0900

    stop中の待機場所変更

--- consai_examples/consai_examples/decisions/side_wing.py
+++ consai_examples/consai_examples/decisions/side_wing.py
@@ -46,6 +46,13 @@ class SideWingDecision(DecisionBase):
         p2_y = (1.8 + 0.5) * (-1 if self._wing_id.value > 0 else 1)
         self._operator.move_to_line_to_defend_our_goal(robot_id, p1_x, p1_y, p2_x, p2_y)
 
+    def _defend_our_half_way(self, robot_id):
+        # Half-wayラインの自陣側で待機
+        target_x = -1.0
+        target_y = 4.0 * (-1 if self._wing_id.value > 0 else 1)
+        self._operator.move_to_reflect_shoot_to_their_goal(
+                    robot_id, target_x, target_y)
+
     def _offend_upper_defense_area(self, robot_id):
         # 相手フィールドで待機する
         p1_x = 2.0
@@ -56,7 +63,7 @@ class SideWingDecision(DecisionBase):
 
     def stop(self, robot_id):
         if self._act_id != self.ACT_ID_STOP:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_STOP
 
     def inplay(self, robot_id):
@@ -67,22 +74,22 @@ class SideWingDecision(DecisionBase):
 
     def our_pre_kickoff(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_KICKOFF:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_PRE_KICKOFF
 
     def our_kickoff(self, robot_id):
         if self._act_id != self.ACT_ID_KICKOFF:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_KICKOFF
 
     def their_pre_kickoff(self, robot_id):
         if self._act_id != self.ACT_ID_PRE_KICKOFF:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_PRE_KICKOFF
 
     def their_kickoff(self, robot_id):
         if self._act_id != self.ACT_ID_KICKOFF:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_KICKOFF
 
     def our_pre_penalty(self, robot_id):
@@ -117,22 +124,22 @@ class SideWingDecision(DecisionBase):
 
     def our_direct(self, robot_id):
         if self._act_id != self.ACT_ID_DIRECT:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_DIRECT
 
     def their_direct(self, robot_id):
         if self._act_id != self.ACT_ID_DIRECT:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_DIRECT
 
     def our_indirect(self, robot_id):
         if self._act_id != self.ACT_ID_INDIRECT:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_INDIRECT
 
     def their_indirect(self, robot_id):
         if self._act_id != self.ACT_ID_INDIRECT:
-            self._defend_upper_defense_area(robot_id)
+            self._defend_our_half_way(robot_id)
             self._act_id = self.ACT_ID_INDIRECT
 
     def our_ball_placement(self, robot_id, placement_pos):
