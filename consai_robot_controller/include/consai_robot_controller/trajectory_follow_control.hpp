cd9912d7f7f9a483f67009d62290657302cc29ce
tilt-silvie <tilt.silvie@gmail.com>
2023-12-24 00:18:21 +0900

    基本的なクラスとコンストラクタを実装

---
+++ consai_robot_controller/include/consai_robot_controller/trajectory_follow_control.hpp
@@ -0,0 +1,59 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "consai_visualizer_msgs/msg/objects.hpp"
+#include "consai_visualizer_msgs/msg/shape_line.hpp"
+
+class Pose2D {
+public:
+  double x;
+  double y;
+  double theta;
+
+  Pose2D();
+  Pose2D(double x, double y, double theta);
+};
+
+class TimeStamp {
+public:
+    /* 原点となる時刻　UnixTime形式 */
+    uint64_t start_time_ms;
+    /* start_time_ms からの相対的な時刻 */
+    uint64_t timestamp_ms;
+
+    TimeStamp();
+    TimeStamp(uint64_t start_time_ms, uint64_t timestamp_ms);
+};
+
+class Pose2DStamped {
+public:
+  Pose2D pose;
+  TimeStamp timestamp;
+
+  Pose2DStamped();
+  Pose2DStamped(Pose2D pose, TimeStamp timestamp);
+};
+
+class Trajectory {
+public:
+  Trajectory();
+  Trajectory(std::vector<Pose2D> poses, uint64_t start_time_ms, uint64_t dt_ms);
+    
+  std::vector<Pose2DStamped> poses;
+};
+
+
+
+
+
+/**
+ * Visualize 関連
+*/
+class TrajectoryVisualizer {
+    public:
+        /**
+         * @brief TrajectoryからObjetcsを生成するクラスメソッド
+         * @param trajectory
+         * @return Objects
+         */ 
+        static consai_visualizer_msgs::msg::Objects createObjectsFromTrajectory(const Trajectory& trajectory);
+};
---
+++ consai_robot_controller/src/trajectory_follow_control.cpp
@@ -0,0 +1,77 @@
+#include "consai_robot_controller/trajectory_follow_control.hpp"
+#include <cstdint>
+
+// Pose2D クラスの定義
+Pose2D::Pose2D() {
+    this->x = 0.0;
+    this->y = 0.0;
+    this->theta = 0.0;
+}
+
+Pose2D::Pose2D(double x, double y, double theta) {
+    this->x = x;
+    this->y = y;
+    this->theta = theta;
+}
+
+// TimeStamp クラスの定義
+TimeStamp::TimeStamp() {
+    this->start_time_ms = 0;
+    this->timestamp_ms = 0;
+}
+
+TimeStamp::TimeStamp(uint64_t start_time_ms, uint64_t timestamp_ms) {
+    this->start_time_ms = start_time_ms;
+    this->timestamp_ms = timestamp_ms;
+}
+
+// Pose2DStamped クラスの定義
+Pose2DStamped::Pose2DStamped() {
+    this->pose = Pose2D();
+    this->timestamp = TimeStamp();
+}
+
+Pose2DStamped::Pose2DStamped(Pose2D pose, TimeStamp timestamp) {
+    this->pose = pose;
+    this->timestamp = timestamp;
+}
+
+// Trajectory クラスの定義
+Trajectory::Trajectory() {
+    this->poses = std::vector<Pose2DStamped>();
+}
+
+Trajectory::Trajectory(std::vector<Pose2D> poses, uint64_t start_time_ms, uint64_t dt_ms) {
+    this->poses = std::vector<Pose2DStamped>();
+
+    for (auto i = 0; i < poses.size(); i++) {
+        Pose2DStamped pose2d_stamped = Pose2DStamped(poses[i], TimeStamp(start_time_ms, dt_ms * i));
+        this->poses.push_back(pose2d_stamped);
+    }
+}
+
+// TrajectoryVisualizer クラスの定義
+consai_visualizer_msgs::msg::Objects TrajectoryVisualizer::createObjectsFromTrajectory(const Trajectory& trajectory) {
+    consai_visualizer_msgs::msg::Objects objects;
+
+    objects.layer = "trajectory";
+    objects.sub_layer = "trajectory";
+
+    // trajectory is visualised by objects.lines
+    for (auto i = 0; i < trajectory.poses.size() - 1; i++) {
+        consai_visualizer_msgs::msg::ShapeLine line;
+
+        line.p1.x = trajectory.poses[i].pose.x;
+        line.p1.y = trajectory.poses[i].pose.y;
+        line.p2.x = trajectory.poses[i + 1].pose.x;
+        line.p2.y = trajectory.poses[i + 1].pose.y;
+        line.size = 1;
+        line.color.red = 1.0;
+        line.color.green = 0.0;
+        line.color.blue = 0.0;
+        line.color.alpha = 1.0;
+        objects.lines.push_back(line);
+    }
+
+    return objects;
+}
