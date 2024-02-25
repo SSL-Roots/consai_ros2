6a22ce2639d8b42154def6993bcc351d035fdd2b
tilt-silvie <tilt.silvie@gmail.com>
2024-02-25 15:04:18 +0000

    chatterを表示するサンプルを実装

--- consai_web_ui/consai_web_ui/app/page.tsx
+++ consai_web_ui/consai_web_ui/app/page.tsx
@@ -2,9 +2,10 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Rosconnection } from "@/app/lib/RosConnection";
+import ROSLIB from "roslib";
 
 export default function Home() {
   const [ros, setRos] = useState(null);
@@ -13,6 +14,33 @@ export default function Home() {
     <>
       <Rosconnection rosUrl="ws://127.0.0.1:9090" setRos={setRos} />
       <h1>Hello consai web ui</h1>
+      <MsgBox ros={ros} />
     </>
+  );
+}
+
+
+const MsgBox = ({ros}) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!ros) return;
+
+    const listener = new ROSLIB.Topic({
+      ros: ros,
+      name: "/chatter",
+      messageType: "std_msgs/String",
+    });
+
+    listener.subscribe((message) => {
+      console.log("Received message on " + listener.name + ": " + message.data);
+      setMessage(message.data);
+    });
+  }, [ros]);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
   );
 }
