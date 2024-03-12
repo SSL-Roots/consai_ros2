9ec691121fe1eddebb442c879cd553531e9364c7
tilt-silvie <tilt.silvie@gmail.com>
2024-03-12 00:20:59 +0000

    ださいcursor selectorを削除

--- consai_web_ui/consai_web_ui/pages/index.tsx
+++ consai_web_ui/consai_web_ui/pages/index.tsx
@@ -25,57 +25,11 @@ export default function Home() {
       <Rosconnection port={9090} setRos={setRos} />
       <h1>Hello consai web ui</h1>
 
-      <CurosrSelector />
       {/* <MsgBox ros={ros} /> */}
       <BirdView ros={ros} />
     </>
   );
 }
-
-const CurosrSelector = () => {
-  const [age, setAge] = useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-  const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const yellows = ids.map((id) => {
-    return (
-      <MenuItem key={id} value={`y${id}`}>
-        Y{id}
-      </MenuItem>
-    );
-  });
-
-  const blues = ids.map((id) => {
-    return (
-      <MenuItem key={id} value={`b${id}`}>
-        B{id}
-      </MenuItem>
-    );
-  });
-
-  return (
-    <>
-      <FormControl size="small">
-        <InputLabel id="demo-simple-select-label">Cursor</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={"ball"}>Ball</MenuItem>
-          {yellows}
-          {blues}
-        </Select>
-      </FormControl>
-      Age: {age}
-    </>
-  );
-};
 
 const MsgBox = ({ ros }) => {
   const [message, setMessage] = useState("");
