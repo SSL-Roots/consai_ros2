0306a619fb0ab0b401a04b7265d7788416444e27
tilt-silvie <tilt.silvie@gmail.com>
2024-03-12 00:47:46 +0000

    ToggleButtonでのカーソルセレクタを実装

--- consai_web_ui/consai_web_ui/components/birdview/birdview.tsx
+++ consai_web_ui/consai_web_ui/components/birdview/birdview.tsx
@@ -5,6 +5,16 @@ import Ball from "./ball";
 import DetectionTracked from "./detectionTracked";
 import ROSLIB from "roslib";
 
+import * as React from "react";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+import { css } from "@emotion/react";
+
 type BirdViewProps = {
   ros: ROSLIB.Ros | null;
 };
@@ -34,29 +44,93 @@ const BirdView = ({ ros }: BirdViewProps) => {
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      scale={{ x: canvasSize.scale, y: canvasSize.scale }}
-    >
-      <Layer offset={{ x: -canvasSize.width / 2, y: -canvasSize.height / 2 }}>
-        <Group
-          rotation={0}
-          onDblClick={(e) => {
-            console.log(e.currentTarget.getRelativePointerPosition());
-            const pos: Vector2D = {
-              x: e.currentTarget.getRelativePointerPosition().x,
-              y: e.currentTarget.getRelativePointerPosition().y,
-            };
-            publishBallReplacement(pos);
-          }}
-        >
-          <Field ros={ros} />
-          <DetectionTracked ros={ros} />
-        </Group>
-      </Layer>
-    </Stage>
+    <>
+      <CursorSelector />
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        scale={{ x: canvasSize.scale, y: canvasSize.scale }}
+      >
+        <Layer offset={{ x: -canvasSize.width / 2, y: -canvasSize.height / 2 }}>
+          <Group
+            rotation={0}
+            onDblClick={(e) => {
+              console.log(e.currentTarget.getRelativePointerPosition());
+              const pos: Vector2D = {
+                x: e.currentTarget.getRelativePointerPosition().x,
+                y: e.currentTarget.getRelativePointerPosition().y,
+              };
+              publishBallReplacement(pos);
+            }}
+          >
+            <Field ros={ros} />
+            <DetectionTracked ros={ros} />
+          </Group>
+        </Layer>
+      </Stage>
+    </>
   );
 };
 
+const CursorSelector = () => {
+  const [alignment, setAlignment] = React.useState<string | null>("left");
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  const buttonBaseStyle = css({
+    width: "2em",
+    height: "2em",
+    textAlign: "center",
+    lineHeight: "2em",
+    borderRadius: "50%",
+  });
+  const blueStyle = css(buttonBaseStyle, {
+    backgroundColor: "#5FB2DB",
+  });
+  const yellowStyle = css(buttonBaseStyle, {
+    backgroundColor: "#DED943",
+  });
+  const ballStyle = css(buttonBaseStyle, {
+    backgroundColor: "#DB7A44",
+  });
+
+  const ids = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const blueButtons = ids.map((id, index) => (
+    <ToggleButton key={index} value={`blue-${id}`} aria-label={`blue-${id}`}>
+      <div css={blueStyle}>{id}</div>
+    </ToggleButton>
+  ));
+  const yellowButtons = ids.map((id, index) => (
+    <ToggleButton
+      key={index}
+      value={`yellow-${id}`}
+      aria-label={`yellow-${id}`}
+    >
+      <div css={yellowStyle}>{id}</div>
+    </ToggleButton>
+  ));
+
+  return (
+    <>
+      <ToggleButtonGroup
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="cursor selector"
+      >
+        <ToggleButton value="ball" aria-label="ball">
+          <div css={ballStyle}>B</div>
+        </ToggleButton>
+
+        {blueButtons}
+        {yellowButtons}
+      </ToggleButtonGroup>
+    </>
+  );
+};
 export default BirdView;
