b6dc0bfc82dd95551309e86ae34c28f8de04229b
Shota Aoki <macakasit@gmail.com>
2024-03-20 20:53:50 +0900

    Add scenario tests for penalty. (#187)
    * Add tests for penalty
    
    * Add tests for penalty

--- tests/test_scenario_penalty.py
+++ tests/test_scenario_penalty.py
@@ -16,9 +16,13 @@ import math
 import time
 
 from rcst.communication import Communication
+from rcst import calc
+from rcst.ball import Ball
+from rcst.robot import RobotDict
 
 
 # TODO: Implement a motion dribbling the ball to the opponent's side.
+# Issue: https://github.com/SSL-Roots/consai_ros2/issues/186
 
 # def test_our_penalty_shoot(rcst_comm: Communication):
 #     rcst_comm.send_empty_world()
@@ -49,3 +53,115 @@ def test_their_penalty_defend(rcst_comm: Communication):
     time.sleep(5)
 
     assert rcst_comm.observer.goal().ball_has_been_in_negative_goal() is False
+
+
+# def blue_robot_are_behind_ball(
+#         ball: Ball, blue_robots: RobotDict, yellow_robots: RobotDict) -> bool:
+#     DISTANCE = 1.0  # meters
+#     KICKER_ID = 1
+
+#     for robot in blue_robots.values():
+#         if robot.id == KICKER_ID:
+#             continue
+
+#         if robot.x > ball.x - DISTANCE:
+#             return False
+
+#     return True
+
+# TODO: Fix the issue: https://github.com/SSL-Roots/consai_ros2/issues/185
+# def test_robots_be_behind_ball(rcst_comm: Communication):
+#     rcst_comm.send_empty_world()
+#     for i in range(11):
+#         rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
+#     time.sleep(3)  # Wait for the robots to be placed.
+
+#     # Kicker robot.
+#     rcst_comm.send_blue_robot(1, -2.2, 0.0, math.radians(0))
+#     rcst_comm.send_ball(-2, 0)
+
+#     rcst_comm.change_referee_command('STOP', 3.0)
+#     rcst_comm.change_referee_command('PREPARE_PENALTY_BLUE', 0.0)
+
+#     rcst_comm.observer.customized().register_sticky_true_callback(
+#         "test", blue_robot_are_behind_ball)
+
+#     success = False
+#     for _ in range(10):
+#         if rcst_comm.observer.customized().get_result("test"):
+#             success = True
+#             break
+#         time.sleep(1)
+#     assert success is True
+
+
+def test_robots_be_behind_ball_for_their_penalty(rcst_comm: Communication):
+    rcst_comm.send_empty_world()
+    for i in range(11):
+        rcst_comm.send_blue_robot(i, 1.0, 3.0 - i * 0.5, math.radians(0))
+
+    rcst_comm.send_ball(2, 0)
+
+    rcst_comm.change_referee_command('STOP', 3.0)
+    rcst_comm.change_referee_command('PREPARE_PENALTY_YELLOW', 0.0)
+
+    def blue_robot_are_behind_ball(
+            ball: Ball, blue_robots: RobotDict, yellow_robots: RobotDict) -> bool:
+        DISTANCE = 1.0  # meters
+        GOALIE_ID = 0
+
+        for robot in blue_robots.values():
+            if robot.id == GOALIE_ID:
+                continue
+
+            if robot.x < ball.x + DISTANCE:
+                return False
+        return True
+
+    rcst_comm.observer.customized().register_sticky_true_callback(
+        "test", blue_robot_are_behind_ball)
+
+    success = False
+    for _ in range(10):
+        if rcst_comm.observer.customized().get_result("test"):
+            success = True
+            break
+        time.sleep(1)
+    assert success is True
+
+
+def test_goalie_on_the_goal_line(rcst_comm: Communication):
+    rcst_comm.send_empty_world()
+    rcst_comm.send_blue_robot(0, -4, 0.0, math.radians(0))
+
+    rcst_comm.send_ball(2, 0)
+
+    rcst_comm.change_referee_command('STOP', 3.0)
+    rcst_comm.change_referee_command('PREPARE_PENALTY_YELLOW', 0.0)
+
+    def goalie_on_the_goal_line(
+            ball: Ball, blue_robots: RobotDict, yellow_robots: RobotDict) -> bool:
+        GOALIE_ID = 0
+        HALF_FIELD_LENGTH = -6.0  # meters
+        HALF_GOAL_WIDTH = 0.9  # meters
+        ROBOT_RADIUS = 0.09  # meters
+
+        for robot in blue_robots.values():
+            if robot.id != GOALIE_ID:
+                continue
+
+            if abs(robot.x - HALF_FIELD_LENGTH) < ROBOT_RADIUS and \
+                    abs(robot.y) < HALF_GOAL_WIDTH:
+                return True
+        return False
+
+    rcst_comm.observer.customized().register_sticky_true_callback(
+        "test", goalie_on_the_goal_line)
+
+    success = False
+    for _ in range(10):
+        if rcst_comm.observer.customized().get_result("test"):
+            success = True
+            break
+        time.sleep(1)
+    assert success is True
