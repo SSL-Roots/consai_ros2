8cb8337f9f18b6525d8e431e4227fdfc4756525d
Shota Aoki <macakasit@gmail.com>
2024-04-13 09:00:16 +0900

    Rotate around ball at dribble (#215)

--- consai_robot_controller/src/tactic/dribble_tactics.cpp
+++ consai_robot_controller/src/tactic/dribble_tactics.cpp
@@ -25,7 +25,7 @@ static constexpr double ROBOT_RADIUS = 0.09;  // ロボットの半径
 static constexpr double DRIBBLE_CATCH = 1.0;
 static constexpr double DRIBBLE_RELEASE = 0.0;
 static constexpr auto APPROACH = "APPROACH";
-static constexpr auto CATCH = "CATCH";
+// static constexpr auto CATCH = "CATCH";
 static constexpr auto ROTATE = "ROTATE";
 static constexpr auto CARRY = "CARRY";
 static constexpr auto RELEASE = "RELEASE";
@@ -48,53 +48,79 @@ DribbleTactics::DribbleTactics()
       data_set.set_parsed_dribble_power(DRIBBLE_RELEASE);
 
       if (tools::is_same(robot_pose, new_pose, DISTANCE_THRESHOLD, THETA_THRESHOLD)) {
-        return CATCH;
+        return ROTATE;
       }
 
       return APPROACH;
     };
 
-  tactic_functions_[CATCH] = [this](TacticDataSet & data_set) -> TacticName {
-      // ドリブラーを駆動させながら前進する
-      // ボールをキャッチするため、一定時間は行動を継続する
-      constexpr auto CATCHING_TIME = 1.0;  // seconds
+  // tactic_functions_[CATCH] = [this](TacticDataSet & data_set) -> TacticName {
+  //     // ドリブラーを駆動させながら前進する
+  //     // ボールをキャッチするため、一定時間は行動を継続する
+  //     constexpr auto CATCHING_TIME = 1.0;  // seconds
 
-      const auto robot_pose = tools::pose_state(data_set.get_my_robot());
-      const auto ball_pose = tools::pose_state(data_set.get_ball());
+  //     const auto robot_pose = tools::pose_state(data_set.get_my_robot());
+  //     const auto ball_pose = tools::pose_state(data_set.get_ball());
 
-      const tools::Trans trans_BtoR(ball_pose, tools::calc_angle(ball_pose, robot_pose));
-      const auto new_pose = trans_BtoR.inverted_transform(ROBOT_RADIUS * 0.0, 0.0, -M_PI);
+  //     const tools::Trans trans_BtoR(ball_pose, tools::calc_angle(ball_pose, robot_pose));
+  //     const auto new_pose = trans_BtoR.inverted_transform(ROBOT_RADIUS * 0.0, 0.0, -M_PI);
 
-      data_set.set_parsed_pose(new_pose);
-      data_set.set_parsed_dribble_power(DRIBBLE_CATCH);
+  //     data_set.set_parsed_pose(new_pose);
+  //     data_set.set_parsed_dribble_power(DRIBBLE_CATCH);
 
-      const auto robot_id = data_set.get_my_robot().robot_id.id;
-      const auto elapsed =
-        chrono::duration<double>(chrono::system_clock::now() - tactic_time_[robot_id]).count();
+  //     const auto robot_id = data_set.get_my_robot().robot_id.id;
+  //     const auto elapsed =
+  //       chrono::duration<double>(chrono::system_clock::now() - tactic_time_[robot_id]).count();
 
-      if (elapsed > CATCHING_TIME) {
-        return ROTATE;
-      }
+  //     if (elapsed > CATCHING_TIME) {
+  //       return ROTATE;
+  //     }
 
-      return CATCH;
-    };
+  //     return CATCH;
+  //   };
 
   tactic_functions_[ROTATE] = [this](TacticDataSet & data_set) -> TacticName {
       // ボールを中心にロボットが回転移動し、ボールと目標値の直線上に移動する
+      // 目的位置をしっかり狙ったら、次の行動に移行する
       constexpr auto DISTANCE_THRESHOLD = 0.05;  // meters
       const auto THETA_THRESHOLD = tools::to_radians(5.0);
+      const auto OMEGA_THRESHOLD = 0.1;  // rad/s
 
       const auto robot_pose = tools::pose_state(data_set.get_my_robot());
       const auto ball_pose = tools::pose_state(data_set.get_ball());
       const auto target_pose = data_set.get_target();
 
+      // ボールから目標位置を結ぶ直線上で、ロボットがボールを見ながら、ボールの周りを旋回する
       const tools::Trans trans_BtoT(ball_pose, tools::calc_angle(ball_pose, target_pose));
-      const auto new_pose = trans_BtoT.inverted_transform(-ROBOT_RADIUS, 0.0, 0.0);
+
+      const auto robot_pose_BtoT = trans_BtoT.transform(robot_pose);
+      const auto angle_robot_position = tools::calc_angle(State(), robot_pose_BtoT);
+
+      const auto ADD_ANGLE = tools::to_radians(60.0);
+      const auto AIM_ANGLE_THRETHOLD = tools::to_radians(15.0);
+
+      double rotation_radius = ROBOT_RADIUS * 2.0;
+
+      State new_pose;
+      if (std::fabs(angle_robot_position) > M_PI - AIM_ANGLE_THRETHOLD) {
+        // ボールの裏に回ったら、直進する
+        new_pose = trans_BtoT.inverted_transform(-ROBOT_RADIUS, 0.0, 0.0);
+      } else {
+        // ボールの周りを旋回する
+        const auto theta = angle_robot_position + std::copysign(ADD_ANGLE, angle_robot_position);
+        double pos_x = rotation_radius * std::cos(theta);
+        double pos_y = rotation_radius * std::sin(theta);
+        new_pose = trans_BtoT.inverted_transform(pos_x, pos_y, theta + M_PI);
+      }
 
       data_set.set_parsed_pose(new_pose);
       data_set.set_parsed_dribble_power(DRIBBLE_CATCH);
 
-      if (tools::is_same(robot_pose, new_pose, DISTANCE_THRESHOLD, THETA_THRESHOLD)) {
+      // ロボットが目標姿勢に近づき、回転速度が小さくなったら次の行動に移行
+      const auto robot_vel = tools::velocity_state(data_set.get_my_robot());
+      if (tools::is_same(robot_pose, new_pose, DISTANCE_THRESHOLD, THETA_THRESHOLD) &&
+        robot_vel.theta < OMEGA_THRESHOLD)
+      {
         return CARRY;
       }
 
