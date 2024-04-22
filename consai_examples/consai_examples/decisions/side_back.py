d336d2dba24d2e86b96bfab76fe7a7a6b194e1e6
nabekuro <nabekuro0104@gmail.com>
2024-04-22 12:59:09 +0900

    割り込む対象をボールじゃなくてゴールに修正

--- consai_examples/consai_examples/decisions/side_back.py
+++ consai_examples/consai_examples/decisions/side_back.py
@@ -34,6 +34,8 @@ class SideBackDecision(DecisionBase):
     def __init__(self, robot_operator, field_observer, side_id: SideBackID):
         super().__init__(robot_operator, field_observer)
         self._side_id = side_id
+        self._wait_target_x = -4.5
+        self._wait_target_y = 3.0
         self._distance_from = 0.25
         self._our_penalty_pos_x = -self._PENALTY_WAIT_X
         self._our_penalty_pos_y = 4.5 - 0.3 * (3.0 + self._side_id.value)
@@ -51,7 +53,7 @@ class SideBackDecision(DecisionBase):
         if self._field_observer.side_back_target().has_target(SIDE_ID) and without_mark is False:
             target_id = self._field_observer.side_back_target().get_target_id(SIDE_ID)
             operation = Operation().move_on_line(
-                TargetXY.their_robot(target_id), TargetXY.ball(),
+                TargetXY.their_robot(target_id), TargetXY.our_goal(),
                 distance_from_p1=self._distance_from, target_theta=TargetTheta.look_ball())
             operation = operation.with_ball_receiving()
         else:
@@ -64,8 +66,8 @@ class SideBackDecision(DecisionBase):
 
     def _get_side_target_xy(self):
         # 待機場所をそれぞれの関数で算出する
-        target_x = -3.5
-        target_y = 0
+        target_x = self._wait_target_x
+        target_y = self._wait_target_y
         if self._side_id == SideBackID.SIDE1:
             return self._get_side1_target_xy()
         elif self._side_id == SideBackID.SIDE2:
@@ -73,13 +75,13 @@ class SideBackDecision(DecisionBase):
         return target_x, target_y
 
     def _get_side1_target_xy(self):
-        target_x = -3.5
-        target_y = 3.0
+        target_x = self._wait_target_x
+        target_y = self._wait_target_y
         return target_x, target_y
 
     def _get_side2_target_xy(self):
-        target_x = -3.5
-        target_y = -3.0
+        target_x = self._wait_target_x
+        target_y = -self._wait_target_y
         return target_x, target_y
 
     def stop(self, robot_id):
