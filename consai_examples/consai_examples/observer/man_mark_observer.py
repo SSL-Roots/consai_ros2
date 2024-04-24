389fbf574a23e69e5de43d382b56341c7dd44459
ShotaAk <macakasit@gmail.com>
2024-04-25 07:12:26 +0900

    refactoring

--- consai_examples/consai_examples/observer/man_mark_observer.py
+++ consai_examples/consai_examples/observer/man_mark_observer.py
@@ -19,6 +19,7 @@ from consai_visualizer_msgs.msg import Objects
 from consai_visualizer_msgs.msg import ShapeLine
 import math
 
+IDType = int
 OurIDType = int
 TheirIDType = int
 InValidID = -1
@@ -31,33 +32,41 @@ class ManMarkObserver:
         self._our_active_bot_ids: list[OurIDType] = []
 
     def update(self, ball: PosVel,
-               our_robots: dict[OurIDType, PosVel], their_robots: dict[TheirIDType, PosVel]) -> None:
-        DETECTION_THRESHOLD_X = 0.0
-
+               our_robots: dict[OurIDType, PosVel],
+               their_robots: dict[TheirIDType, PosVel]) -> None:
         self._remove_non_exist_robots_from_mark(our_robots, their_robots)
 
-        target_their_bots: dict[TheirIDType: PosVel] = {}
-        for their_bot_id, their_bot in their_robots.items():
-            if their_bot.pos().x > DETECTION_THRESHOLD_X:
-                self._remove_mark_via_their_id(their_bot_id)
-                continue
+        # マーク条件に該当するロボットを抽出
+        # ここの条件を変更することで、より賢いマークを実現できる
+        markable_ids = self._detect_their_bots_in_our_side(their_robots)
 
-            if self._is_marked(their_bot_id):
-                continue
+        non_markable_ids = set(their_robots.keys()) - set(markable_ids)
+        for non_markable_id in non_markable_ids:
+            self._remove_mark_via_their_id(non_markable_id)
 
-            target_their_bots[their_bot_id] = their_bot
+        # 既にマークされているロボットをマーク対象リストから削除
+        already_marked_ids = [their_id for their_id in markable_ids if self._is_marked(their_id)]
+        markable_ids = list(set(markable_ids) - set(already_marked_ids))
 
         for our_bot_id in self._our_active_bot_ids:
+            if len(markable_ids) == 0:
+                return
+
             if our_bot_id not in our_robots.keys():
                 continue
 
             if not self._is_free_bot(our_bot_id):
                 continue
 
+            # 対象のロボットに最も近いロボットをマークに割り当てる
             our_bot = our_robots[our_bot_id]
-            nearest_their_bot_id = self._search_nearest_bot_id(our_bot.pos(), target_their_bots)
+            nearest_their_bot_id = self._search_nearest_bot_id(
+                our_bot.pos(), their_robots, markable_ids)
+
+            # アサインに成功したら、マーク対象リストからIDを削除
             if self._assign_mark(our_bot_id, nearest_their_bot_id):
-                target_their_bots.pop(nearest_their_bot_id)
+                if nearest_their_bot_id in markable_ids:
+                    markable_ids.remove(nearest_their_bot_id)
 
     def set_our_active_bot_ids(self, our_active_bot_ids: list[OurIDType]) -> None:
         self._our_active_bot_ids = our_active_bot_ids
@@ -69,7 +78,8 @@ class ManMarkObserver:
             return InValidID
 
     def to_visualize_msg(
-            self, our_robots: dict[OurIDType, PosVel], their_robots: dict[TheirIDType, PosVel]) -> Objects:
+            self, our_robots: dict[OurIDType, PosVel],
+            their_robots: dict[TheirIDType, PosVel]) -> Objects:
         # アクティブロボットとマーク対象の相手ロボットを結ぶ直線を描画する
 
         vis_objects = Objects()
@@ -97,18 +107,33 @@ class ManMarkObserver:
 
         return vis_objects
 
+    def _detect_their_bots_in_our_side(
+            self, their_robots: dict[OurIDType, PosVel]) -> list[OurIDType]:
+        DETECTION_THRESHOLD_X = 0.0
+
+        bot_ids = []
+        for bot_id, bot in their_robots.items():
+            if bot.pos().x < DETECTION_THRESHOLD_X:
+                bot_ids.append(bot_id)
+
+        return bot_ids
+
     def _remove_mark_via_their_id(self, their_id: TheirIDType) -> None:
         # 削除するキーを一時リストに保存
-        to_remove = [our_id for our_id, their_id_ in self._mark_dict.items() if their_id == their_id_]
+        to_remove = [
+            our_id for our_id, their_id_ in self._mark_dict.items() if their_id == their_id_]
         # 実際に削除
         for our_id in to_remove:
             self._mark_dict.pop(our_id)
 
     def _remove_non_exist_robots_from_mark(
-            self, our_robots: dict[OurIDType, PosVel], their_robots: dict[TheirIDType, PosVel]) -> None:
+            self, our_robots: dict[OurIDType, PosVel],
+            their_robots: dict[TheirIDType, PosVel]) -> None:
         # 存在しないキーを一時リストに保存
-        to_remove_via_theirs = [our_id for our_id, their_id in self._mark_dict.items() if their_id not in their_robots]
-        to_remove_via_ours = [our_id for our_id in self._mark_dict.keys() if our_id not in our_robots]
+        to_remove_via_theirs = [
+            our_id for our_id, their_id in self._mark_dict.items() if their_id not in their_robots]
+        to_remove_via_ours = [
+            our_id for our_id in self._mark_dict.keys() if our_id not in our_robots]
         # 実際に削除
         for our_id in to_remove_via_theirs + to_remove_via_ours:
             self._mark_dict.pop(our_id)
@@ -122,34 +147,18 @@ class ManMarkObserver:
     def _our_free_bot_ids(self) -> list[OurIDType]:
         return list(set(self._our_active_bot_ids) - set(self._mark_dict.keys()))
 
-    def _search_nearest_our_bot(
-            self, their_bot_pos: State2D,
-            our_target_ids: list[OurIDType], our_robots: dict[OurIDType, PosVel]) -> OurIDType:
-
-        nearest_id = InValidID
-        nearest_dist = math.inf
-
-        for our_id in our_target_ids:
-            if our_id not in our_robots.keys():
-                continue
-
-            our_pos = our_robots[our_id].pos()
-            dist = tool.get_distance(our_pos, their_bot_pos)
-
-            if dist < nearest_dist:
-                nearest_id = our_id
-                nearest_dist = dist
-
-        return nearest_id
-
     def _search_nearest_bot_id(
             self, my_pos: State2D,
-            target_robots: dict[OurIDType, PosVel]) -> OurIDType:
+            target_robots: dict[IDType, PosVel],
+            markable_ids: list[IDType]) -> IDType:
 
         nearest_id = InValidID
         nearest_dist = math.inf
 
         for target_id, target_bot in target_robots.items():
+            if target_id not in markable_ids:
+                continue
+
             target_pos = target_bot.pos()
             dist = tool.get_distance(my_pos, target_pos)
 
