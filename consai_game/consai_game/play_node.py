a1095432b4dd01d1297b90ff6fe0d024c7be7b10
ShotaAk <macakasit@gmail.com>
2025-03-01 11:22:45 +0900

    Add play_node

---
+++ consai_game/consai_game/play_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+# Copyright 2025 Roots
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from consai_game.utils.process_info import process_info
+from rclpy.node import Node
+
+
+class PlayNode(Node):
+    def __init__(self, update_hz: float = 10):
+        super().__init__('play_node')
+        assert update_hz > 0
+        self.timer = self.create_timer(1.0/update_hz, self.update)
+
+    def update(self):
+        self.get_logger().info(f'Play update, {process_info()}')
