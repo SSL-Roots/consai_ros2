909dda40a47a86fd2ba1f48df5d225f7b22b42da
ShotaAk <macakasit@gmail.com>
2025-04-07 22:22:54 +0900

    Implement play_condition with arg example

---
+++ consai_game/consai_game/play/conditions/ball_conditions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+# Copyright 2025 Roots
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from consai_game.world_model.world_model import WorldModel
+from consai_game.core.play.play_condition import PlayCondition
+from consai_game.utils.geometry import state2d_norm
+
+
+class BallConditions:
+    @staticmethod
+    def velocity_is_lower_than(threshold: float) -> PlayCondition:
+        def condition(world_model: WorldModel) -> bool:
+            velocity = state2d_norm(world_model.ball.vel)
+            return velocity < threshold
+        return PlayCondition(condition)
