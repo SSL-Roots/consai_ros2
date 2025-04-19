61498a66742e75b5b6cd1fdba2ea4df0099ccaee
ShotaAk <macakasit@gmail.com>
2025-04-19 11:04:21 +0900

    Refactor

--- consai_game/consai_game/core/role_assignment/methods/by_cost.py
+++ consai_game/consai_game/core/role_assignment/methods/by_cost.py
@@ -16,6 +16,7 @@ from consai_game.core.role_assignment.methods.base import RoleAssignmentBase
 from consai_game.core.tactic.role import RoleConst
 from consai_game.core.tactic.tactic_base import TacticBase
 from consai_game.world_model.world_model import WorldModel
+from consai_msgs.msg import MotionCommand
 from consai_tools.geometry.geometry_tools import get_distance
 from scipy.optimize import linear_sum_assignment
 from typing import List
@@ -31,8 +32,7 @@ class ByCost(RoleAssignmentBase):
 
     def __init__(self):
         super().__init__()
-        self.GAIN_TARGET_POSE = 100
-        self.GAIN_BALL = 0.5
+        self.GAIN_TARGET_POSE = 1.0
 
     def assign(
         self,
@@ -70,49 +70,22 @@ class ByCost(RoleAssignmentBase):
                 # TODO: 例外処理を入れたほうが良い
                 tactic[0].reset(robot_id)
 
-                target_pos = tactic[0].run(world_model).desired_pose
+                command = tactic[0].run(world_model)
 
-                # 目標位置との距離コスト
-                cost_target = self.GAIN_TARGET_POSE * get_distance(robot_pos, target_pos)
-                # ボールとの距離コスト
-                cost_ball = self.GAIN_BALL * get_distance(world_model.ball.pos, robot_pos)
+                if command.mode == MotionCommand.MODE_DIRECT_VELOCITY:
+                    # 速度制御の場合は、コストを0にする
+                    cost_target = 0.0
+                else:
+                    # 目標位置との距離コスト
+                    cost_target = self.GAIN_TARGET_POSE * get_distance(robot_pos, command.desired_pose)
 
-                cost_matrix[i, j] = cost_target + cost_ball
+                cost_matrix[i, j] = cost_target
 
         # 最適化問題を解く
         robot_index, role_index = linear_sum_assignment(cost_matrix)
 
-        # デバッグ表示
-        for robot, role in zip(robot_index, role_index):
-            print(f"robot {robots_without_goalie[robot]} -> role {role}")
-
         # id_listにロボットIDをセットする
         for i, robot in enumerate(robot_index):
             id_list[role_index[i] + 1] = robots_without_goalie[robot]
-
-        # for robot_id in world_model.robot_activity.our_visible_robots:
-
-        #     if robot_id not in id_list:
-        #         # goalie_idが存在しない場合は、最初にセットする
-        #         if id_list[0] == RoleConst.INVALID_ROLE_ID:
-        #             id_list[0] = robot_id
-        #         break
-
-        # # 新しく現れたロボットを抽出する
-        # new_robots = [
-        #     robot_id
-        #     for robot_id in world_model.robot_activity.ordered_our_visible_robots
-        #     if robot_id not in id_list
-        # ]
-
-        # # 新しく現れたロボットを、空いている役割に割り当てる
-        # for new_robot_id in new_robots:
-        #     for i in range(1, role_num):
-        #         if id_list[i] == RoleConst.INVALID_ROLE_ID:
-        #             id_list[i] = new_robot_id
-        #             break
-
-        # # 割当を記憶する
-        # self.prev_id_list = id_list.copy()
 
         return id_list
