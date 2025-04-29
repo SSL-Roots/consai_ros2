674dff9d0c0a0726081afeefadc105337fc3f5ec
Yamam0t0 <yuuto06148041@icloud.com>
2025-04-29 18:03:47 +0900

    ボールが通るラインにかぶっていたらdesired_poseを上書きする

--- consai_game/consai_game/tactic/wrapper/open_kick_line.py
+++ consai_game/consai_game/tactic/wrapper/open_kick_line.py
@@ -17,12 +17,16 @@ from consai_msgs.msg import MotionCommand
 from consai_game.world_model.world_model import WorldModel
 from consai_game.core.tactic.tactic_base import TacticBase
 
+from consai_tools.geometry import geometry_tools as tool
+
 
 class OpenKickLine(TacticBase):
     """キックラインを開くWrapperTactic.
 
     OpenKickLine(tactic=Position()) のように使用する
     """
+
+    AVOID_RADIUS = 0.3  # ボールを避ける半径 [m]
 
     def __init__(self, tactic=TacticBase):
         """inner_tacticを初期化する関数."""
@@ -35,13 +39,30 @@ class OpenKickLine(TacticBase):
         self.inner_tactic.reset(robot_id)
 
     def run(self, world_model: WorldModel) -> MotionCommand:
-        """パスラインを開くようにdesired_poseを上書きする."""
+        """キックラインを開くようにdesired_poseを上書きする."""
         command = self.inner_tactic.run(world_model)
 
-        # パスラインを開く処理を追加
+        # キックラインを開く処理を追加
         # ここでは、desired_poseを適切な位置に変更するロジックを実装する
+        shoot_target = world_model.kick_target.best_shoot_target
+        pass_target = world_model.kick_target.best_pass_target
+        ball_pos = world_model.ball.pos
 
-        # stateを上書きする
-        self.state = self.inner_tactic.state
+        # 目標位置がシュートラインに干渉しているか計算
+        is_shoot_line_blocked = tool.is_on_line(
+            pose=command.desired_pose, line_pose1=ball_pos, line_pose2=shoot_target, tolerance=self.AVOID_RADIUS
+        )
+
+        # 目標位置がパスラインに干渉しているか計算
+        is_pass_line_blocked = tool.is_on_line(
+            pose=command.desired_pose, line_pose1=ball_pos, line_pose2=pass_target, tolerance=self.AVOID_RADIUS
+        )
+
+        # パスを受けるロボットが自分でなく、シュートラインもしくはパスラインに干渉している場合
+        if self.robot_id != pass_target.robot_id and (is_shoot_line_blocked or is_pass_line_blocked):
+            # パスラインを開く
+            command.desired_pose.y = command.desired_pose.y + self.AVOID_RADIUS
+            # stateを上書きする
+            self.state = self.inner_tactic.state
 
         return command
