7c4a23c5156bc3ae629d79484183ae7bb64418c5
nabekuro <38589340+nabekuro0104@users.noreply.github.com>
2025-05-02 23:37:38 +0900

    マンマークの位置がペナルティエリアに入らないように＆相手のプレースメント中のゴーリーがペナルティエリアから出ないように (#436)
    * マンマークの目標位置がペナルティエリアに入らないように
    
    * their_ball_placementの修正

--- consai_game/consai_game/tactic/man_mark.py
+++ consai_game/consai_game/tactic/man_mark.py
@@ -18,6 +18,8 @@ class ManMark(TacticBase):
         super().__init__()
         self.target_robot_id = target_robot_id
         self.mark_distance = mark_distance
+        # ペナルティエリア際に移動するためのマージン
+        self.area_margin = 0.15
 
     def reset(self, robot_id: int):
         self.robot_id = robot_id
@@ -27,6 +29,9 @@ class ManMark(TacticBase):
         command = MotionCommand()
         command.robot_id = self.robot_id
         command.mode = MotionCommand.MODE_NAVI
+
+        # フィールド情報取得
+        field = world_model.field
 
         if self.target_robot_id in world_model.robot_activity.their_visible_robots:
             target_robot = world_model.robots.their_robots.get(self.target_robot_id)
@@ -47,6 +52,26 @@ class ManMark(TacticBase):
         # 敵との間合いを取る位置を計算（敵から mark_distance 離れたところ）
         mark_x = target_robot.pos.x - self.mark_distance * math.cos(angle_to_target)
         mark_y = target_robot.pos.y - self.mark_distance * math.sin(angle_to_target)
+
+        # マーク位置がペナルティエリア内に入ってしまっているかどうかを判定
+        mark_pos_in_penalty_area = (
+            math.fabs(mark_y) < field.half_penalty_width and mark_x < -field.half_length + field.penalty_depth
+        )
+
+        # マーク位置がペナルティエリア内に入ってしまった
+        if mark_pos_in_penalty_area:
+            # ゴール正面側
+            if target_robot.pos.x > -field.half_length + field.penalty_depth:
+                # ペナルティエリアより外に出す
+                mark_x = max(mark_x, -field.half_length + field.penalty_depth + self.area_margin)
+            # ゴール両側面側
+            else:
+                # ペナルティエリアより外に出す
+                mark_y = (
+                    max(mark_y, field.half_penalty_width + self.area_margin)
+                    if mark_y > 0
+                    else min(mark_y, -field.half_penalty_width - self.area_margin)
+                )
 
         # 目的地設定
         command.desired_pose = State2D()
