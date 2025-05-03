a043ef348c65fafb16afe061fdbd6d0598a8729f
nabekuro <nabekuro0104@gmail.com>
2025-05-03 10:56:24 +0900

    ゴーリーポジション修正

--- consai_game/consai_game/tactic/goalie_positioning.py
+++ consai_game/consai_game/tactic/goalie_positioning.py
@@ -20,11 +20,12 @@ DefendGoal Tactic.
 
 from consai_game.core.tactic.tactic_base import TacticBase
 from consai_game.world_model.world_model import WorldModel
-
+from consai_game.world_model.field_model import Field
+from consai_game.world_model.ball_model import BallModel
 from consai_msgs.msg import MotionCommand
-from consai_msgs.msg import State2D
 
-from consai_tools.geometry import geometry_tools as tool
+# from consai_msgs.msg import State2D
+# from consai_tools.geometry import geometry_tools as tool
 
 import math
 
@@ -48,53 +49,109 @@ class GoaliePositioning(TacticBase):
         command = MotionCommand()
         command.robot_id = self.robot_id
 
+        field = world_model.field
+        ball = world_model.ball
         # ボールの位置を取得
-        ball_pos = world_model.ball.pos
+        # ball_pos = world_model.ball.pos
 
         # ボールの保持状態を取得
-        ball_holder = world_model.ball_activity.ball_holder
+        # ball_holder = world_model.ball_activity.ball_holder
 
-        if ball_holder is None or ball_holder.is_our_team:
-            # ボールを誰も保持していないとき、もしくは味方ロボットが保持しているとき
+        #########################################################
+        # 敵方向の考慮は一旦無視します
+        #########################################################
 
-            # x座標をゴール前にする
-            x = -world_model.field.half_length + self.ROBOT_RADIUS
-            # y座標をボールと同じ位置にする
-            y = ball_pos.y
+        # if ball_holder is None or ball_holder.is_our_team:
+        #     # ボールを誰も保持していないとき、もしくは味方ロボットが保持しているとき
+        #     x, y = self.get_goalie_position_free_ball(field, ball)
 
-        elif not ball_holder.is_our_team:
-            # ボールを相手ロボットが保持しているとき
-            robot = ball_holder.robot
+        # elif not ball_holder.is_our_team:
+        #     # ボールを相手ロボットが保持しているとき
+        #     robot = ball_holder.robot
 
-            # ゴールラインについての座標
-            l1p1 = world_model.field_points.our_goal_top
-            l1p2 = world_model.field_points.our_goal_bottom
+        #     # ゴールラインについての座標
+        #     l1p1 = world_model.field_points.our_goal_top
+        #     l1p2 = world_model.field_points.our_goal_bottom
 
-            # 相手ロボットと相手ロボットの向いている線分の座標
-            l2p1 = robot.pos
-            l2p2 = State2D()
-            l2p2.x = robot.pos.x + 10 * math.cos(robot.pos.theta)
-            l2p2.y = robot.pos.y + 10 * math.sin(robot.pos.theta)
+        #     # 相手ロボットと相手ロボットの向いている線分の座標
+        #     l2p1 = robot.pos
+        #     l2p2 = State2D()
+        #     l2p2.x = robot.pos.x + 10 * math.cos(robot.pos.theta)
+        #     l2p2.y = robot.pos.y + 10 * math.sin(robot.pos.theta)
 
-            # ロボットの方向とゴールラインの交点
-            intersecsion = tool.get_line_intersection(
-                line1_pose1=l1p1, line1_pose2=l1p2, line2_pose1=l2p1, line2_pose2=l2p2, is_on_line1_check=False
-            )
+        #     # ロボットの方向とゴールラインの交点
+        #     intersecsion = tool.get_line_intersection(
+        #         line1_pose1=l1p1, line1_pose2=l1p2, line2_pose1=l2p1, line2_pose2=l2p2, is_on_line1_check=False
+        #     )
 
-            if intersecsion is None:
-                # x座標をゴール前にする
-                x = -world_model.field.half_length + self.ROBOT_RADIUS
-                # y座標をボールと同じ位置にする
-                y = ball_pos.y
-            else:
-                x = intersecsion.x
-                y = intersecsion.y
+        #     if intersecsion is None:
+        #         # x座標をゴール前にする
+        #         x = -world_model.field.half_length + self.ROBOT_RADIUS
+        #         # y座標をボールと同じ位置にする
+        #         y = ball_pos.y
+        #     else:
+        #         x = intersecsion.x
+        #         y = intersecsion.y
 
-        # ゴールからはみ出ないようにclamp
-        y = max(min(y, world_model.field.half_goal_width), -world_model.field.half_goal_width)
+        # # ゴールからはみ出ないようにclamp
+        # y = max(min(y, world_model.field.half_goal_width), -world_model.field.half_goal_width)
+
+        # ボールを誰も保持していないとき、もしくは味方ロボットが保持しているとき
+        x, y = self.get_goalie_position_free_ball(field, ball)
 
         command.desired_pose.x = x
         command.desired_pose.y = y
         command.desired_pose.theta = 0.0
 
         return command
+
+    def get_goalie_position_free_ball(self, field: Field, ball: BallModel):
+        """
+        ボールを誰も保持していないとき、もしくは味方ロボットが保持しているときのゴーリーのポジションを生成する
+        ボールの位置とゴール両端へのベクトルの長さを比較して、短い方のベクトルの方向にゴーリーを配置する
+        """
+
+        # 1. ゴールの上端・下端
+        goal_x = -field.half_length + self.ROBOT_RADIUS
+        goal_y_top = field.half_goal_width
+        goal_y_bottom = -field.half_goal_width
+
+        # 2. ボール位置
+        ball_x = ball.pos.x
+        ball_y = ball.pos.y
+
+        # 3. BGt, BGbベクトルと長さ
+        vec_BGt = [goal_x - ball_x, goal_y_top - ball_y]
+        vec_BGb = [goal_x - ball_x, goal_y_bottom - ball_y]
+        len_BGt = math.hypot(vec_BGt[0], vec_BGt[1])
+        len_BGb = math.hypot(vec_BGb[0], vec_BGb[1])
+
+        if len_BGt < len_BGb:
+            # BGtが短い場合
+            base_len = len_BGt
+            # BGb方向の単位ベクトル
+            vec_BGb_n = [vec_BGb[0] / len_BGb, vec_BGb[1] / len_BGb]
+            # Gb'を計算
+            G_other_x = ball_x + vec_BGb_n[0] * base_len
+            G_other_y = ball_y + vec_BGb_n[1] * base_len
+            # 底辺はGt-Gb'
+            base_goal_x = goal_x
+            base_goal_y = goal_y_top
+        else:
+            # BGbが短い場合
+            base_len = len_BGb
+            # BGt方向の単位ベクトル
+            vec_BGt_n = [vec_BGt[0] / len_BGt, vec_BGt[1] / len_BGt]
+            # Gt'を計算
+            G_other_x = ball_x + vec_BGt_n[0] * base_len
+            G_other_y = ball_y + vec_BGt_n[1] * base_len
+            # 底辺はGb-Gt'
+            base_goal_x = goal_x
+            base_goal_y = goal_y_bottom
+
+        # 4. 底辺の中点
+        mid_x = (base_goal_x + G_other_x) / 2
+        mid_y = (base_goal_y + G_other_y) / 2
+
+        # 5. ゴーリーの配置座標
+        return mid_x, mid_y
