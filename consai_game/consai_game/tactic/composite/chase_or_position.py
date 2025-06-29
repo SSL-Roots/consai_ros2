67a2a74f8404c55bc89f8b5c8aaf1ab82e7f16cd
ShotaAk <macakasit@gmail.com>
2025-06-29 14:05:21 +0900

    Refactor chase or position

--- consai_game/consai_game/tactic/composite/chase_or_position.py
+++ consai_game/consai_game/tactic/composite/chase_or_position.py
@@ -18,7 +18,7 @@
 PositionとChaseBallのTacticを組み合わせたTactic
 """
 
-from consai_game.core.tactic.tactic_base import TacticBase
+from consai_game.core.tactic.composite_tactic_base import CompositeTacticBase
 from consai_game.tactic.position import Position
 from consai_game.tactic.chase_ball import ChaseBall
 from consai_game.world_model.world_model import WorldModel
@@ -26,7 +26,7 @@ from consai_game.world_model.world_model import WorldModel
 from consai_msgs.msg import MotionCommand
 
 
-class ChaseOrPosition(TacticBase):
+class ChaseOrPosition(CompositeTacticBase):
     """
     ボールに一番近ければボールを取りに行く、そうでなければ指定した位置に移動する.
 
@@ -35,36 +35,17 @@ class ChaseOrPosition(TacticBase):
 
     def __init__(self, x=0.0, y=0.0, theta=0.0):
         """Initialize the DefendGoal tactic."""
-        super().__init__()
+        super().__init__(tactic_chase=ChaseBall(), tactic_position=Position(x, y, theta))
+
         self.x = x
         self.y = y
         self.theta = theta
-
-        self.tactic_chase = ChaseBall()
-        self.tactic_position = Position(x, y, theta)
-
-    def reset(self, robot_id: int) -> None:
-        """Reset the tactic state for the specified robot."""
-        super().reset(robot_id)
-
-        # 所有するTacticも初期化する
-        self.tactic_chase.reset(robot_id)
-        self.tactic_position.reset(robot_id)
-
-    def exit(self):
-        super().exit()
-
-        # 所有するTacticもexitする
-        self.tactic_chase.exit()
-        self.tactic_position.exit()
 
     def run(self, world_model: WorldModel) -> MotionCommand:
         """Run the tactic and return a MotionCommand based on the ball's position and movement."""
         if world_model.robot_activity.our_robots_by_ball_distance[0] == self.robot_id:
             # ボールに近い場合はボールを追いかける
-            command = self.tactic_chase.run(world_model)
+            return self.run_sub_tactic(self.tactic_chase, world_model)
         else:
             # ボールに近くない場合は指定した位置に移動する
-            command = self.tactic_position.run(world_model)
-
-        return command
+            return self.run_sub_tactic(self.tactic_position, world_model)
